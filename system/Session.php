9db9eac900d5ee83d1a21b17cc41ac915c7e414f
Jac <lu7766lu7766@gmail.com>
2021-04-15 22:05:14 +0800

    設定session過期時間

--- system/Session.php
+++ system/Session.php
@@ -1,6 +1,25 @@
 <?php
 // unit sec
-$defaultTimeout = 60 * 60 * 2;
-ini_set("session.cookie_lifetime", getenv2("SESSION_TIMEOUT", $defaultTimeout));
-ini_set("session.gc_maxlifetime", $defaultTimeout);
-session_start();
+// $timeout = getenv2("SESSION_TIMEOUT", 60 * 60 * 2) ;
+// ini_set("session.cookie_lifetime", $timeout);
+// ini_set("session.gc_maxlifetime", $timeout);
+// session_start();
+
+function start_session($expire = 0)
+{
+    if ($expire == 0) {
+        $expire = ini_get('session.gc_maxlifetime');
+    } else {
+        ini_set('session.gc_maxlifetime', $expire);
+    }
+
+    if (empty($_COOKIE['PHPSESSID'])) {
+        session_set_cookie_params($expire);
+        session_start();
+    } else {
+        session_start();
+        setcookie('PHPSESSID', session_id(), time() + $expire);
+    }
+}
+
+start_session(getenv2("SESSION_TIMEOUT", 60 * 60 * 2));
